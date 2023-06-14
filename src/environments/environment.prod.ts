6e9311a679bbb3c2b5cdc37326f56b31f9dcf94a
cecabrerac <ccabrera9@hotmail.com>
2023-06-14 11:20:34 -0500

    Cambio URL localhost

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  serverURL: 'https://apptrial.herokuapp.com/',
+  // serverURL: 'https://apptrial.herokuapp.com/',
+  serverURL: 'https://localhost:3000/',
 };
